cc6a10c5d462a13c4f656650520b65132ea65224
ViniCod <viniciusprates.6798@aluno.saojudas.br>
2021-03-13 13:36:29 -0300

    Adição dos arquivos do Body-Parser

--- projetos/teste-http/server.js
+++ projetos/teste-http/server.js
@@ -2,9 +2,21 @@ const http = require('http');
 const express = require('express');
 const app = express();
 const porta = 3000;
+const clientes = [
+    {
+        id: 1,
+        nome: 'João',
+        email: 'joao@email.com'
+    },
+    {
+        id: 2,
+        nome: 'Cristina',
+        email: 'cristina@email.com'
+    }
+];
 app.set('port', porta);
-app.get('/teste', (req, res, next) => {
-    res.send("Hello World!");
+app.get('/clientes', (req, res, next) => {
+    res.json(clientes);
 });
 const server = http.createServer(app);
 server.listen(3000);
