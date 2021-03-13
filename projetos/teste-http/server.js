453331a2661fc0eb318f732c4d672d552db1fec9
ViniCod <viniciusprates.6798@aluno.saojudas.br>
2021-03-13 14:19:47 -0300

    Criação do método post com um contador de id

--- projetos/teste-http/server.js
+++ projetos/teste-http/server.js
@@ -1,7 +1,10 @@
 const http = require('http');
 const express = require('express');
 const app = express();
+const bodyParser = require ('body-parser');
+app.use (bodyParser.json());
 const porta = 3000;
+let contador = 3;
 const clientes = [
     {
         id: 1,
@@ -17,6 +20,12 @@ const clientes = [
 app.set('port', porta);
 app.get('/clientes', (req, res, next) => {
     res.json(clientes);
+});
+app.post('/clientes', (req, res, next) => {
+    const cliente = req.body;
+    clientes.push({id: contador += 1, nome: cliente.nome, email: cliente.email});
+    console.log(clientes);
+    res.end();
 });
 const server = http.createServer(app);
 server.listen(3000);
