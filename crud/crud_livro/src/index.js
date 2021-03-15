92feddad39c72e283b2a56ba5746bbfd602b7790
ViniCod <viniciusprates.6798@aluno.saojudas.br>
2021-03-15 20:14:58 -0300

    Adição dos métodos post e get

--- crud/crud_livro/src/index.js
+++ crud/crud_livro/src/index.js
@@ -11,3 +11,24 @@ const porta = 3000;
 app.set('port', porta);
 const server = http.createServer(app);
 server.listen(3000);
+const cont = 1;
+const livros = [
+    {
+        id: 1,
+        título: 'Solo Leveling',
+        descricao: 'Do mais fraco ao mais poderoso! Uma história de perseverança e superação pessoal!',
+        edicao: 'Volume 1',
+        autor: 'Chugong'
+    }
+];
+
+app.set('port', porta);
+app.post('/livros', (req, res, next) => {
+    const livro = req.body;
+    livros.push({id: contador += 1, título: livro.titulo, descrição: livro.descricao, edição: livro.edicao, autor: livro.autor});
+    console.log(livros);
+    res.status(201).json(livros);
+})
+app.get('livros', (req, res, next) => {
+    res.json(livros);
+});
