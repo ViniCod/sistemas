8bf1f489dfa555ac0aa060d9c3efbf7c0f3f413e
ViniCod <viniciusprates.6798@aluno.saojudas.br>
2021-03-15 19:55:48 -0300

    Imports das bibliotecas instaladas anteriormente

---
+++ crud/crud_livro/src/index.js
@@ -0,0 +1,13 @@
+//importa os módulos http e express
+const http = require('http');
+const express = require('express');
+//constrói um objeto express
+const app = express();
+//importa o body-parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+//configura a porta do servidor e o coloca em execução.
+const porta = 3000;
+app.set('port', porta);
+const server = http.createServer(app);
+server.listen(3000);
